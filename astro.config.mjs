74bc9c18b6823f91361aee6ca8a9b80212ee626e
Pechlivanis Dimitrios <dimipech@hotmail.com>
2023-02-19 11:24:41 +0200

    Install & configure  markdown render package called astropub@md

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config'
 import image from '@astrojs/image'
+import markdownIntegration from '@astropub/md'
 
 export default defineConfig({
 	integrations: [
 		image({
 			serviceEntryPoint: '@astrojs/image/sharp',
 		}),
+		markdownIntegration(),
 	],
+	markdown: {
+		remarkPlugins: [],
+		rehypePlugins: [],
+		// syntaxHighlight: 'shiki'
+		// syntaxHighlight: 'prism'
+	},
 })
